787a4e501de7b5f22a5916c99fc358f01fb80626
developernilesh <nileshmukherjee2017@gmail.com>
2024-05-21 20:23:06 +0530

    debug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Employees, Layout, Navbar } from "../components";
 
 const Home = () => {
+  console.log("In the home page");
   return (
     <Layout>
       <Navbar/>
